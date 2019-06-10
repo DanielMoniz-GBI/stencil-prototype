fb3dea43ed67d01943640cb9af9a27ceee7c733a
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-06-10 11:18:51 -0400

    Allow for overriding default click functionality.

--- src/components/gb-tile/gb-tile.tsx
+++ src/components/gb-tile/gb-tile.tsx
@@ -23,11 +23,20 @@ export class GBTile {
             </slot>
             <div class="price">Product price</div>
             <div class="description">Product description</div>
-            <span onClick={this.buyNow}>
-              <slot name="buy-button">
-                <button class="buy">Buy now!</button>
-              </slot>
-            </span>
+            <slot name="buy-button-all">
+              <span onClick={this.buyNow}>
+                <slot name="buy-button">
+                  <button class="buy">Buy now!</button>
+                </slot>
+              </span>
+            </slot>
+            <slot name="add-to-cart-all">
+              <span onClick={this.addToCart}>
+                <slot name="add-to-cart">
+                  <button class="add-to-cart">Add to Cart</button>
+                </slot>
+              </span>
+            </slot>
           </slot>
         </main>
       </div>
@@ -36,5 +45,9 @@ export class GBTile {
 
   buyNow() {
     console.log('>> Clicked Buy Now button. (default functionality)')
+  }
+
+  addToCart() {
+    console.log('>> Clicked Add to Cart button. (default functionality)')
   }
 }
